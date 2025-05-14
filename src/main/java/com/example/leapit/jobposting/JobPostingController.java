e1de66fdc6be154b82b596bce69a2f191f62073f
harimmon <bhr0502@gmail.com>
2025-05-14 20:44:58 +0900

    채용공고 등록 완료

--- src/main/java/com/example/leapit/jobposting/JobPostingController.java
+++ src/main/java/com/example/leapit/jobposting/JobPostingController.java
@@ -1,10 +1,31 @@
 package com.example.leapit.jobposting;
 
+import com.example.leapit._core.util.Resp;
+import com.example.leapit.user.User;
+import com.example.leapit.user.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class JobPostingController {
     private final JobPostingService jobPostingService;
+    private final UserRepository userRepository;
+
+    private final HttpSession session;
+
+    @PostMapping("/company/jobposting/save")
+    public ResponseEntity<?> save(@Valid @RequestBody JobPostingRequest.SaveDTO saveDTO, Errors errors) {
+        User user = userRepository.findByUsername("company01");
+        session.setAttribute("sessionUser", user);
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        JobPostingRequest.SaveDTO reqDTO = jobPostingService.save(saveDTO, sessionUser);
+        return Resp.ok(reqDTO);
+    }
 }
--- src/main/java/com/example/leapit/user/UserRepository.java
+++ src/main/java/com/example/leapit/user/UserRepository.java
@@ -8,4 +8,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
     private final EntityManager em;
+
+    public User findByUsername(String username) {
+
+        User userPS = em.createQuery("select u from User u where u.username = :username", User.class)
+                .setParameter("username", username)
+                .getSingleResult();
+        return userPS;
+
+    }
 }
+
