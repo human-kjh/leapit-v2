5db0fe7fa9f1ab2a716ae3e2523b3747dd78d4a3
hahamik <dlapdl0615@gmail.com>
2025-05-19 20:55:11 +0900

    공고현황

--- src/main/java/com/example/leapit/common/enums/BookmarkStatus.java
+++ src/main/java/com/example/leapit/common/enums/BookmarkStatus.java
@@ -4,7 +4,7 @@ public enum BookmarkStatus {
     BOOKMARKED("스크랩됨"),
     NOT_BOOKMARKED("스크랩안됨");
 
-    public final String label;
+    public String label;
 
     BookmarkStatus(String label) {
         this.label = label;
