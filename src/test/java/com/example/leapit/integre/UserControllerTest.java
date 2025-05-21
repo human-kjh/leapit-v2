23bec462e2ca32d6a09203493fb84df4ed7f6bcd
hahamik <dlapdl0615@gmail.com>
2025-05-21 20:54:26 +0900

    1

--- src/test/java/com/example/leapit/integre/UserControllerTest.java
+++ src/test/java/com/example/leapit/integre/UserControllerTest.java
@@ -29,4 +29,313 @@ import static org.hamcrest.Matchers.matchesPattern;
 @Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class UserControllerTest extends MyRestDoc {
+
+    @Autowired
+    private ObjectMapper om;
+    private String personalAccessToken;
+    private String companyAccessToken;
+
+    @BeforeEach
+    public void setUp(){
+        // 테스트 시작 전에 실행 할 코드
+        System.out.println("setUp");
+        User ssar = User.builder().id(1).username("ssar").build();
+        User company02 = User.builder().id(7).username("company02").build();
+        personalAccessToken = JwtUtil.create(ssar);
+        companyAccessToken = JwtUtil.create(company02);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        // 테스트 후 정리할 코드
+        System.out.println("tearDown");
+    }
+
+    @Test
+    public void personal_join_test() throws Exception {
+        // given
+        UserRequest.PersonalJoinDTO reqDTO = new UserRequest.PersonalJoinDTO();
+        reqDTO.setName("가나다");
+        reqDTO.setUsername("admin123");
+        reqDTO.setPassword("Qw12!@34");
+        reqDTO.setEmail("admin@example.com");
+        reqDTO.setContactNumber("010-9999-9999");
+        reqDTO.setBirthDate(LocalDate.of(1990, 1, 1));
+        reqDTO.setRole(Role.PERSONAL);
+
+
+        String requestBody = om.writeValueAsString(reqDTO);
+//        System.out.println(requestBody);
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/personal/join")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        //System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.id").value(9));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.username").value("admin123"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.email").value("admin@example.com"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.role").value("PERSONAL"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.createdAt")
+                .value(Matchers.matchesPattern("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d+$")));
+        actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+
+    @Test
+    public void company_join_test() throws Exception {
+        // given
+        UserRequest.CompanyJoinDTO reqDTO = new UserRequest.CompanyJoinDTO();
+        reqDTO.setUsername("company06");
+        reqDTO.setPassword("Qw12!@34");
+        reqDTO.setEmail("company@example.com");
+        reqDTO.setContactNumber("010-1234-5678");
+        reqDTO.setRole(Role.COMPANY);
+
+
+        String requestBody = om.writeValueAsString(reqDTO);
+//        System.out.println(requestBody);
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/company/join")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        //System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.id").value(9));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.username").value("company06"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.email").value("company@example.com"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.role").value("COMPANY"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.createdAt")
+                .value(Matchers.matchesPattern("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d+$")));
+        actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @Test
+    public void check_username_available_test() throws Exception {
+        // given
+        String username = "ssar";
+
+        String requestBody = om.writeValueAsString(username);
+//        System.out.println(requestBody);
+
+//         when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/api/check-username-available/{username}", username)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        //System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.available").value(false));
+        actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @Test
+    public void login_test() throws Exception {
+        // given
+        UserRequest.LoginDTO reqDTO = new UserRequest.LoginDTO();
+        reqDTO.setUsername("ssar");
+        reqDTO.setPassword("1234");
+        reqDTO.setRole(Role.PERSONAL);
+        reqDTO.setRememberMe(null);
+
+
+        String requestBody = om.writeValueAsString(reqDTO);
+//        System.out.println(requestBody);
+
+//         when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/login")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.accessToken")
+                .value(Matchers.matchesPattern("^[^.]+\\.[^.]+\\.[^.]+$")));
+        actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+
+    @Test
+    public void get_personal_update_form_test() throws Exception {
+        // given
+        Integer userId = 1;
+
+
+        String requestBody = om.writeValueAsString(userId);
+//        System.out.println(requestBody);
+
+//         when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/s/api/personal/user/edit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + personalAccessToken)
+        );
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.id").isNumber());
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.username").value("ssar"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.email").value("ssar@nate.com"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.role").value("PERSONAL"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.createdAt")
+                .value(Matchers.matchesPattern("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d+$")));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.name").value("쌀"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.birthDate").value("2000-01-01"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.contactNumber").value("010-1234-5678"));
+        actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+
+    }
+
+    @Test
+    public void get_company_update_form_test() throws Exception {
+        // given
+        Integer userId = 7;
+
+        String requestBody = om.writeValueAsString(userId);
+//        System.out.println(requestBody);
+
+//         when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/s/api/company/user/edit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + companyAccessToken)
+        );
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.id").isNumber());
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.username").value("company02"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.email").value("company02@nate.com"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.role").value("COMPANY"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.createdAt")
+                .value(Matchers.matchesPattern("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d+$")));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.name").value(Matchers.nullValue()));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.birthDate").value(Matchers.nullValue()));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.contactNumber").value("02-2345-6789"));
+        actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @Test
+    public void company_update_test() throws Exception {
+        // given
+        UserRequest.CompanyUpdateDTO reqDTO = new UserRequest.CompanyUpdateDTO();
+        reqDTO.setNewPassword("Qw12!@34");
+        reqDTO.setConfirmPassword("Qw12!@34");
+        reqDTO.setContactNumber("010-9876-5432");
+        reqDTO.setEmail("company02@naver.com");
+
+
+        String requestBody = om.writeValueAsString(reqDTO);
+        System.out.println(requestBody);
+
+//         when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .put("/s/company/user")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + companyAccessToken)
+        );
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.id").isNumber());
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.username").value("company02"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.email").value("company02@naver.com"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.role").value("COMPANY"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.createdAt")
+                .value(Matchers.matchesPattern("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d+$")));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.name").value(Matchers.nullValue()));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.birthDate").value(Matchers.nullValue()));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.contactNumber").value("010-9876-5432"));
+        actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @Test
+    public void personal_update_test() throws Exception {
+        // given
+        UserRequest.PersonalUpdateDTO reqDTO = new UserRequest.PersonalUpdateDTO();
+        reqDTO.setName("홍길동");
+        reqDTO.setNewPassword("Abcd1234!");
+        reqDTO.setConfirmPassword("Abcd1234!");
+        reqDTO.setEmail("hong.gildong@example.com");
+        reqDTO.setContactNumber("010-1234-5678");
+
+
+        String requestBody = om.writeValueAsString(reqDTO);
+//        System.out.println(requestBody);
+
+//         when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .put("/s/personal/user")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + personalAccessToken)
+        );
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.id").isNumber());
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.username").value("ssar"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.email").value("hong.gildong@example.com"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.role").value("PERSONAL"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.createdAt")
+                .value(Matchers.matchesPattern("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d+$")));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.name").value("홍길동"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.birthDate").value("2000-01-01"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.contactNumber").value("010-1234-5678"));
+        actions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
 }
