bd2b2f676a8fbc9f86a3f8ae4aea351088df56ae
jay <wngml0708@gmail.com>
2025-05-21 15:27:54 +0900

    통합테스트 중

--- src/test/java/com/example/leapit/integre/ResumeControllerTest.java
+++ src/test/java/com/example/leapit/integre/ResumeControllerTest.java
@@ -1,4 +1,80 @@
 package com.example.leapit.integre;
 
+import com.example.leapit._core.util.JwtUtil;
+import com.example.leapit.user.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class ResumeControllerTest {
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private String accessToken;
+
+    @BeforeEach
+    public void setUp(){
+        // 테스트 시작 전에 실행할 코드
+        User ssar = User.builder().id(1).username("ssar").build();
+        accessToken = JwtUtil.create(ssar);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        // 테스트 후 정리할 코드
+    }
+
+    /*
+    기본 세팅
+    @Test
+    public void _test() throws Exception{
+       // given
+
+       // when
+
+       // eye
+
+       // then
+    }
+     */
+
+    // 이력서 목록
+    @Test
+    public void get_list_test() throws Exception{
+        // given
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/api/board/{id}/detail", boardId)
+        );
+        // eye
+
+        // then
+    }
+    // 이력서 삭제
+
+    // 이력서 등록 화면
+
+    // 이력서 등록
+
+    // 이력서 수정
+
+    // 이력서 상세보기
+
+
 }
